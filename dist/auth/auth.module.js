5ed817bf7bda02e38f30a48c835791d91df35fdd
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-28 15:51:12 +0530

    config: Peering PrismaService to AuthModule providers' array

---
+++ dist/auth/auth.module.js
@@ -0,0 +1,32 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AuthModule = void 0;
+const common_1 = require("@nestjs/common");
+const auth_service_1 = require("./auth.service");
+const passport_1 = require("@nestjs/passport");
+const jwt_1 = require("@nestjs/jwt");
+const prisma_service_1 = require("../prisma/prisma.service");
+let AuthModule = class AuthModule {
+};
+AuthModule = __decorate([
+    (0, common_1.Module)({
+        imports: [
+            passport_1.PassportModule,
+            jwt_1.JwtModule.register({
+                secret: process.env.JWT_SECRET,
+                signOptions: {
+                    expiresIn: '1h'
+                }
+            })
+        ],
+        providers: [auth_service_1.AuthService, prisma_service_1.PrismaService]
+    })
+], AuthModule);
+exports.AuthModule = AuthModule;
+//# sourceMappingURL=auth.module.js.map
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt'; 
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '1h'
+      }
+    })
+  ],
+  providers: [AuthService, PrismaService]
 })
 export class AuthModule {}
