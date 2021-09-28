298f0bb6c641a409392476cc9b04cf2c46b89a3a
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-28 12:44:47 +0530

    feat: Peering PrismaService in to users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+
+import { PrismaService } from 'src/prisma/prisma.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, PrismaService]
 })
 export class UsersModule {}
