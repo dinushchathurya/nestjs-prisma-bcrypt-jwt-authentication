bbc76e4f00288ecf3d0f57b69dd1f6c1a0d0288b
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-28 12:56:37 +0530

    feat: Create getSingle user service method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '.prisma/client';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(private readonly prismaService:PrismaService) {}
+
+    async getUser(username: string): Promise<User> {
+
+        const user = await this.prismaService.user.findUnique({
+            where: { username }
+        });
+
+        if(!user) {
+            throw new NotFoundException();
+        }
+
+        delete user.password;
+        return user;
+        
+    }
+}
