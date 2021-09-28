a10b1ca7e65721e2a6ff88c836854260ca596f46
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-28 15:32:24 +0530

    config: Peering auth module in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [UsersModule, PrismaModule, AuthModule],
   controllers: [],
   providers: [],
 })
