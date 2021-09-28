6ad1260576bdbb2bd08fcb3071eb00feddab16fe
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-28 15:22:24 +0530

    refactor: Validate create-user-dto.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,19 @@
+import { IsString, Length } from 'class-validator';
+
 export class CreateUserDto {
+    @IsString()
+    @Length(5, 10)
     username: string;
+
+    @IsString()
+    @Length(6, 12)
     password: string;
+
+    @IsString()
+    @Length(5, 10)
     firstName:string;
+    
+    @IsString()
+    @Length(5, 10)
     lastName: string;
 }
